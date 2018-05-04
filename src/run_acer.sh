55e53e9bc378ac1f2a6226ab3342a51e6aa331d1
Bhargavi Paranjape <bhargavi22294@gmail.com>
2018-05-04 00:06:20 -0400

    run file for acer

---
+++ src/run_acer.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+python -u run.py --agt 16 \
+        --usr 1 \
+        --max_turn 40 \
+        --movie_kb_path ./deep_dialog/data/movie_kb.1k.p \
+        --dqn_hidden_size 80 \
+        --experience_replay_pool_size 1000 \
+        --episodes 4000 \
+        --simulation_epoch_size 100 \
+        --write_model_dir ./deep_dialog/checkpoints/rl_agent/ \
+        --run_mode 10 \
+        --act_level 0 \
+        --slot_err_prob 0.00 \
+        --intent_err_prob 0.00 \
+        --batch_size 64 \
+        --goal_file_path ./deep_dialog/data/user_goals_first_turn_template.part.movie.v1.p \
+        --warm_start 1 \
+        --warm_start_epochs 100 \
+        --gamma 0.99 \
+        --lrate 0.001 \
+        --is_a2c
